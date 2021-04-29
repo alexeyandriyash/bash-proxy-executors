c5fd3413a8892be4c09fde3000c24f4a65e3ca84
aandriya <aandriyash@gmail.com>
2021-04-29 19:58:32 +0300

    Update define-executors.sh

--- define-executors.sh
+++ define-executors.sh
@@ -5,7 +5,7 @@ source $BUILD_SYSTEM_PATH/vars.sh
 # CACHE_PATH -- a path of a dir to store a build system cache (may be any)
 
 # EXAMPLE
-# ENV_TO_BUILD="--env BASE_API_URL=$BASE_API_URL --env BASE_URL=$BASE_URL --env HOST_NAME=$HOST_NAME --env YC_DEPLOYMENT=$YC_DEPLOYMENT --env VEGA_ENV=$VEGA_ENV --env VEGA_SCHEMA_PATH=$VEGA_SCHEMA_PATH --env NPM_AUTH_TOKEN=$NPM_AUTH_TOKEN"
+# ENV_TO_BUILD="--env BASE_API_URL=$BASE_API_URL --env BASE_URL=$BASE_URL --env HOST_NAME=$HOST_NAME --env NPM_AUTH_TOKEN=$NPM_AUTH_TOKEN"
 
 export YARN='eval docker run --rm --name yarn-executor -v $(pwd):/app -v $CACHE_PATH:/usr/local/share/.cache/ $ENV_TO_BUILD $JS_BASE yarn'
 
